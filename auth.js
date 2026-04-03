7ff33132a35daf6dea60d13ef3f95ae7ac09e398
hadmin <hadmin@My-MacBook-Air.local>
2026-04-03 01:04:19 -0700

    removed yoselin

--- auth.js
+++ auth.js
@@ -3,7 +3,6 @@ const users = [
   { username: "admin", password: "12345", firstName: "John", lastName: "Doe" },
   { username: "sarah", password: "98765", firstName: "Sarah", lastName: "Brown" },
   { username: "tyra.jennings@km-textiles.com", password: "LWTsn@70", firstName: "Tyra", lastName: "Jennings" },
-  { username: "yoselin.flores@km-textiles.com", password: "hb7Tq@84", firstName: "Yoselin", lastName: "Flores" },
   { username: "shenaz.fairfield@km-textiles.com", password: "BCx@gh29", firstName: "Shenaz", lastName: "Fairfield" },
   { username: "maria.torres@km-textiles.com", password: "LWTsn@70", firstName: "Maria", lastName: "Torres" },
   { username: "margarita.lizardi@km-textiles.com", password: "LWTsn@70", firstName: "Margarita", lastName: "Lizardi" }
@@ -14,7 +13,6 @@ const allowedUsernames = new Set([
   "admin",
   "sarah",
   "tyra.jennings@km-textiles.com",
-  "yoselin.flores@km-textiles.com",
   "shenaz.fairfield@km-textiles.com",
   "maria.torres@km-textiles.com",
   "margarita.lizardi@km-textiles.com"
